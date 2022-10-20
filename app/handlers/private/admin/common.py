42da48101ba5517b6fa10bcff52322b7c2adda31
flashdrag <flashdrag@gmail.com>
2022-10-20 21:19:05 +0100

    added main menu for cancel from callback

--- app/handlers/private/admin/common.py
+++ app/handlers/private/admin/common.py
@@ -11,17 +11,25 @@ from aiogram.types.bot_command import BotCommand
 from aiogram.types import BotCommandScopeAllGroupChats
 
 
-async def admin_start_process(m: Message):
+async def admin_start_process(m: Message, state: FSMContext):
     await m.answer('The same message as to user',
                    reply_markup=admin_button())
+    await state.reset_state(with_data=True)
 
 
 async def show_admin_panel(m: Message, state: FSMContext):
-    await m.answer('Hello, Admin! Would you like to add, edit or remove any data?',
+    await m.answer('Hello, Admin! Would you like to add, edit or remove the commands?',
                    reply_markup=ikb_admin_panel())
     await state.reset_state(with_data=True)
 
 
+async def show_admin_panel_query(call: CallbackQuery, state: FSMContext):
+    await call.message.answer('Hello, Admin! Would you like to add, edit or remove the commands?',
+                              reply_markup=ikb_admin_panel())
+    await state.reset_state(with_data=True)
+    await call.answer()
+
+'''
 async def set_com(m: Message, state: FSMContext):
     bot = m.bot
     print(bot)
@@ -34,16 +42,25 @@ async def set_com(m: Message, state: FSMContext):
 async def process_commands(m: Message, state: FSMContext):
     print(m.date)
     print(m)
+'''
 
 
 def register_common_private_admin(dp: Dispatcher):
     dp.register_message_handler(
         admin_start_process, CommandStart(),
         chat_type=[ChatType.PRIVATE], is_admin=True)
+
+    dp.register_message_handler(
+        show_admin_panel, commands=['admin', 'cancel'], chat_type=[ChatType.PRIVATE],
+        is_admin=True, state='*')
     dp.register_message_handler(
         show_admin_panel, Text(
-            equals=['/admin', 'cancel', 'Main Menu'], ignore_case=True),
+            equals=['cancel', 'Main Menu'], ignore_case=True),
         chat_type=[ChatType.PRIVATE], is_admin=True, state='*')
 
-    dp.register_message_handler(set_com, commands='setcom')
-    dp.register_message_handler(process_commands, commands=['обед', 'стирка'])
+    dp.register_callback_query_handler(
+        show_admin_panel_query, text='cancel',
+        chat_type=[ChatType.PRIVATE], is_admin=True, state="*")
+
+    # dp.register_message_handler(set_com, commands='setcom')
+    # dp.register_message_handler(process_commands, commands=['обед', 'стирка'])
