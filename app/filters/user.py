863e68ce96536cbeb807b00ab0c93b83e469dd48
flashdrag <flashdrag@gmail.com>
2022-10-24 22:10:29 +0100

    added func description

--- app/filters/user.py
+++ app/filters/user.py
@@ -3,11 +3,15 @@ import typing
 from aiogram.dispatcher.filters import BoundFilter
 from aiogram.types import Message
 
-from bot import my_commands
+from app.utils.set_bot_commands import my_commands
 
 
 class CommandFilter(BoundFilter):
-
+    '''
+    Фильтр получаемый апдейт на Комманду
+    Проводит валидацию на длину и наличие в глобальном словаре кастомных комманд админа,
+    которые сохранены в БД периодически загружаються в этот словарь
+    '''
     async def check(self, obj: typing.Optional[Message]):
         if isinstance(obj, Message) and obj.is_command():
             data = obj.text
--- app/utils/set_bot_commands.py
+++ app/utils/set_bot_commands.py
@@ -1,11 +1,26 @@
 from aiogram.types.bot_command import BotCommand
 
+'''
+key - команда; value - description
+Словарь кастомных комманд админа,
+которые загружаются в главное меню в группе,
+и используються для получения определенного контента с БД,
+записанного админом через админку в боте.
+Полная информация сохранена в БД: command, description, content
+В словарь периодически загружаются комманды и описания,
+для ускорения работы фильтра `CommandFilter()` from app.filters.user,
+который проверяет апдейты на соответствующие комманды
+'''
 
-# Регистрация команд, отображаемых в интерфейсе Telegram
-async def set_commands(dp):
+my_commands: dict = {}
+
+
+# TODO Установить по умолчанию две комманды: start, cancel которые будут работать,
+#  а еще лучше отображаться только в приватном скоупе
+async def set_default_commands(dp):
     commands = [
         BotCommand(command="/start", description="START ✅"),
-        BotCommand(command="/info", description="INFO⚠️"),
+        # BotCommand(command="/info", description="INFO⚠️"),
         BotCommand(command="/cancel", description="CANCEL ❌")
         ]
     await dp.bot.set_my_commands(commands)
